129ddb13e7c04194c79c69e1f508cd28f3bc8824
lijianing <v_lijianing@bilibili.com>
2026-04-20 10:55:54 +0800

    'isvalidbrackets'

--- Learning/exercise/level12/isvalidbrackets.py
+++ Learning/exercise/level12/isvalidbrackets.py
@@ -1,28 +1,11 @@
 def is_valid_brackets(text):
     brackets_list = []
-    stack = []
     brackets = ['(', ')', '[', ']', '{', '}']
     for each in text:
-        if each in brackets :
+        if each in brackets:
             brackets_list.append(each)
 
-    for bracket in brackets_list:
-        if bracket in '([{':
-            # print(bracket)
-            stack.append(bracket)
-        else:
-            if not stack:
-                return False
 
-            top = stack.pop()
-
-            if bracket == ')' and top != '(':
-                return False
-            if bracket == ']' and top != '[':
-                return False
-            if bracket == '}' and top != '{':
-                return False
-
-    return len(stack) == 0
+    return brackets_list
 
 print(is_valid_brackets("is_valid_brackets(text.['apple'])"))
