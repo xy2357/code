04f89e521017db103165f1a0bc7d57ae3926acd8
xy1257 <1909411819@qq.com>
2026-05-09 09:25:05 +0800

    C# stack

---
+++ C#/level1/CSharpMethodExample/CSharpMethodExample/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Calculator.GetConeVolumns(100, 100);
+        }
+    }
+
+    class Calculator
+    {
+        public static double GetCircleArea(double r)
+        {
+            return Math.PI * r;
+        }
+
+        public static double GetCylinderVolumn(double r, double h)
+        {
+            double a = GetCircleArea(r);
+            return a * h;
+        }
+
+        public static double GetConeVolumns(double r, double h)
+        {
+            double cv = GetCylinderVolumn(r, h);
+            return cv * h;
+        }
+    }
+}
