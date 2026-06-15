28d734fb913223909cd753319a51f926278475b9
lijianing <v_lijianing@bilibili.com>
2026-06-15 22:12:33 +0800

    Event

---
+++ C#/level3/Question23/Question23/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Question23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("xy", 100);
+            player.Damage += OnDamage;
+            player.TakeDamage(30);
+        }
+
+        static void OnDamage(int damage,int hp)
+        {
+            Console.WriteLine($"玩家受到了{damage}点伤害,剩余血量{hp}");
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _hp;
+
+        public Player(string name, int hp)
+        {
+            _name = name;
+            _hp = hp;
+        }
+
+        public event Action<int,int> Damage;
+
+        public void TakeDamage(int damage)
+        {
+            _hp -= damage;
+            Damage?.Invoke(damage, _hp);
+        }
+    }
+}
