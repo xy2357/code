ddddba85d880cf2a349b196c5521bb860375321c
lijianing <v_lijianing@bilibili.com>
2026-06-22 20:04:47 +0800

    Exercism_local

---
+++ C#/Exercism_local/Squeaky Clean/Squeaky Clean/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squeaky_Clean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Identifier.Clean("my\0Id");
+            Console.WriteLine(result);
+        }
+    }
+
+    public static class Identifier
+    {
+        public static string Clean(string identifier)
+        {
+            const string str = "CTRL";
+            identifier = identifier.Replace(" ", "_");
+            //identifier = identifier.Replace("\\", str);
+            return identifier;
+        }
+    }
+}
