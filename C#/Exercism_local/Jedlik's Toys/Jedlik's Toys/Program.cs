77d3d3198e35f8383b5e21708a9d806fd1eb3647
xy1257 <1909411819@qq.com>
2026-06-25 09:23:45 +0800

    Jedlik's Toys

---
+++ C#/Exercism_local/Jedlik's Toys/Jedlik's Toys/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedlik_s_Toys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new RemoteControlCar();
+            for (var i = 0; i < 100; i++)
+            {
+                car.Drive();
+            }
+            car.Drive();
+            Console.WriteLine(car.DistanceDisplay());
+            //car.DistanceDisplay();
+        }
+    }
+
+    class RemoteControlCar
+    {
+        private int count = 0;
+        private int battery = 100;
+        public static RemoteControlCar Buy()
+        {
+            return new RemoteControlCar();
+            
+        }
+
+        public string DistanceDisplay()
+        {
+            if (battery > 0)
+            {
+                int distance = 20 * (100 - battery);
+                return "Driven " + distance + " meters";
+            }
+            else
+            {
+                return "Driven " + 100 * 20 + " meters";
+            }
+        }
+
+        public string BatteryDisplay()
+        {
+            if (battery > 0)
+            {
+                return "Battery at " + battery + "%";
+            }
+            else
+            {
+                return "Battery empty";
+            }
+        }
+
+        public void Drive()
+        {
+            if (battery > 0)
+            {
+                battery--;
+            }
+        }
+    }
+}
