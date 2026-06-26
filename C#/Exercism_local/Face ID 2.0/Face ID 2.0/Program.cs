d9ce5303fdaa58d52708566f3062cb322f8ebbd2
lijianing <v_lijianing@bilibili.com>
2026-06-26 22:57:23 +0800

    Exercism

---
+++ C#/Exercism_local/Face ID 2.0/Face ID 2.0/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Face_ID_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class FacialFeatures
+    {
+        public string EyeColor { get; }
+        public decimal PhiltrumWidth { get; }
+
+        public FacialFeatures(string eyeColor, decimal philtrumWidth)
+        {
+            EyeColor = eyeColor;
+            PhiltrumWidth = philtrumWidth;
+        }
+        // TODO: implement equality and GetHashCode() methods
+    }
+
+    public class Identity
+    {
+        public string Email { get; }
+        public FacialFeatures FacialFeatures { get; }
+
+        public Identity(string email, FacialFeatures facialFeatures)
+        {
+            Email = email;
+            FacialFeatures = facialFeatures;
+        }
+        // TODO: implement equality and GetHashCode() methods
+
+        public bool Equailty(string email, FacialFeatures facialFeatures)
+        {
+            if (this.Email == email && this.FacialFeatures == facialFeatures)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class Authenticator
+    {
+
+        Dictionary<int, Identity> identities = new Dictionary<int, Identity>();
+
+        public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB)
+        {
+            if (faceA.EyeColor == faceB.EyeColor && faceA.PhiltrumWidth == faceB.PhiltrumWidth)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsAdmin(Identity identity)
+        {
+            if (identity.Email == "admin@exerc.ism" 
+                && identity.FacialFeatures.EyeColor == "green" 
+                && identity.FacialFeatures.PhiltrumWidth == 0.9m)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Register(Identity identity)
+        {
+            if (!IsRegistered(identity))
+            {
+                identities[identities.GetHashCode()] = identity;
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsRegistered(Identity identity)
+        {
+            if (identities.ContainsKey(identity.GetHashCode()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool AreSameObject(Identity identityA, Identity identityB)
+        {
+            if (identityA.GetHashCode() == identityB.GetHashCode())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ C#/Exercism_local/Logs, Logs, Logs!/Logs, Logs, Logs!/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logs__Logs__Logs_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // TODO: define the 'LogLevel' enum
+    enum LogLevel
+    {
+        Unknown = 0,
+        Trace = 1,
+        Debug = 2,
+        Info = 4,
+        Warning = 5,
+        Error = 6,
+        Fatal = 42,
+    }
+
+    static class LogLine
+    {
+        public static LogLevel ParseLogLevel(string logLine)
+        {
+            if (logLine.Contains("TRC"))
+            {
+                return LogLevel.Trace;
+            }
+            else if(logLine.Contains("DBG"))
+            {
+                return LogLevel.Debug;
+            }
+            else if (logLine.Contains("INF"))
+            {
+                return LogLevel.Info;
+            }
+            else if (logLine.Contains("WRN"))
+            {
+                return LogLevel.Warning;
+            }
+            else if (logLine.Contains("ERR"))
+            {
+                return LogLevel.Error;
+            }
+            else if(logLine.Contains("FTL"))
+            {
+                return LogLevel.Fatal; 
+            }
+            else
+            {
+                return LogLevel.Unknown;
+            }
+        }
+
+        public static string OutputForShortLog(LogLevel logLevel, string message)
+        {
+            return $"{(int)logLevel}:{message}";
+        }
+    }
+}
