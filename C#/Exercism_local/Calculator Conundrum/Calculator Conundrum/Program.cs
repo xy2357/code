53ebb27906f0e3f91d344626b55cfa73347782ec
李佳宁 <lijianing@Mac.local>
2026-06-29 00:20:48 +0800

    Exercism

---
+++ C#/Exercism_local/Calculator Conundrum/Calculator Conundrum/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Calculator_Conundrum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+    public static class SimpleCalculator
+    {
+        public static string Calculate(int operand1, int operand2, string? operation)
+        {
+            
+            if (operation is null)
+            {
+                throw new ArgumentNullException(nameof(operation));
+            }
+
+            if (operation == "")
+            {
+                throw new ArgumentException(nameof(operation));
+            }
+
+            try
+            {
+                if (operation == "*")
+                {
+                    return $"{operand1} * {operand2} = {operand1 * operand2}";
+                }
+                else if (operation == "/")
+                {
+                    if (operand2 == 0)
+                    {
+                        return "Division by zero is not allowed.";
+                    }
+
+                    return $"{operand1} / {operand2} = {operand1 / operand2}";
+                }
+                else if (operation == "+")
+                {
+                    return $"{operand1} + {operand2} = {operand1 + operand2}";
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(operation));
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                return "Division by zero is not allowed.";
+            }
+        }
+    }
+}
---
+++ C#/Exercism_local/FaceID 2.0 2/FaceID 2.0 2/Program.cs
@@ -0,0 +1,94 @@
+﻿public class FacialFeatures
+{
+    public string EyeColor { get; }
+    public decimal PhiltrumWidth { get; }
+
+    public FacialFeatures(string eyeColor, decimal philtrumWidth)
+    {
+        EyeColor = eyeColor;
+        PhiltrumWidth = philtrumWidth;
+    }
+    // TODO: implement equality and GetHashCode() methods
+    protected bool Equals(FacialFeatures other)
+    {
+        if (EyeColor == other.EyeColor && PhiltrumWidth == other.PhiltrumWidth)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((FacialFeatures) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(EyeColor, PhiltrumWidth);
+    }
+}
+
+public class Identity
+{
+    public string Email { get; }
+    public FacialFeatures FacialFeatures { get; }
+
+    public Identity(string email, FacialFeatures facialFeatures)
+    {
+        Email = email;
+        FacialFeatures = facialFeatures;
+    }
+    // TODO: implement equality and GetHashCode() methods
+    protected bool Equals(Identity other)
+    {
+        return (Email == other.Email && FacialFeatures.Equals(other.FacialFeatures)) ;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Identity) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Email, FacialFeatures);
+    }
+}
+
+public class Authenticator
+{
+    
+    private readonly Identity admin = new Identity("admin@exerc.ism", new FacialFeatures("green",0.9m));
+    private readonly HashSet<Identity> identities = new HashSet<Identity>();
+    public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB)
+    {
+        return faceA.Equals(faceB);
+    }
+
+    public bool IsAdmin(Identity identity)
+    {
+        return identity.Equals(admin);
+    }
+
+    public bool Register(Identity identity)
+    {
+        return identities.Add(identity);
+    }
+
+    public bool IsRegistered(Identity identity)
+    {
+        return identities.Contains(identity);
+    }
+
+    public static bool AreSameObject(Identity identityA, Identity identityB)
+    {
+        return ReferenceEquals(identityA, identityB);
+    }
+}
