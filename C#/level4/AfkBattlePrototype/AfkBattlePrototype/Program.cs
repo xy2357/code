b6e2fa379b6cc933a183b105fb428c2db7c476ca
xy1257 <1909411819@qq.com>
2026-08-03 09:28:43 +0800

    level4

---
+++ C#/level4/AfkBattlePrototype/AfkBattlePrototype/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Formats.Asn1;
+
+namespace AfkBattlePrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character hero1 = new Character("hero1", 100, 20, 10);
+            Character hero2 = new Character("hero2", 80, 15, 12);
+            hero1.TakeDamage(hero2);
+            Console.WriteLine(hero2.Hp);
+        }
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+        public Character(string name, int hp, int attack, int defense)
+        {
+            Name = name;
+            Hp = hp;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public void TakeDamage(Character target)
+        {
+            target.Hp -= Attack;
+        }
+
+        public bool IsDead()
+        {
+            if (this.Hp < 0) return true;
+            return false;
+        }
+    }
+}
---
+++ C#/level4/Remote Control Car/Remote Control Car/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remote_Control_Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteControlCar remoteControlCar = new RemoteControlCar();
+            remoteControlCar.Drive();
+            remoteControlCar.Drive();
+            remoteControlCar.Drive();
+            string distanceDisplay = remoteControlCar.DistanceDisplay();
+            string batteryDisplay = remoteControlCar.BatteryDisplay();
+            Console.WriteLine(distanceDisplay);
+            Console.WriteLine(batteryDisplay);
+        }
+    }
+
+    class RemoteControlCar
+    {
+        private int count = 0;
+        private int battery = 100;
+        public static RemoteControlCar Buy()
+        {
+            return new RemoteControlCar();
+
+        }
+
+        public string DistanceDisplay()
+        {
+            if (battery > 0)
+            {
+                int distance = 20 * (100 - battery);
+                return "Driven " + distance + " meters";
+            }
+            else
+            {
+                return "Driven " + 100 * 20 + " meters";
+            }
+        }
+
+        public string BatteryDisplay()
+        {
+            if (battery > 0)
+            {
+                return "Battery at " + battery + "%";
+            }
+            else
+            {
+                return "Battery empty";
+            }
+        }
+
+        public void Drive()
+        {
+            if (battery > 0)
+            {
+                battery--;
+            }
+        }
+    }
+}
